062a43cd18f1ff7afe0f13a0c2358079878d9e8b
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-12-29 23:49:45 +0100

    Sort products by name in Cart

--- code-challenge/src/containers/CartContainer/CartContainer.js
+++ code-challenge/src/containers/CartContainer/CartContainer.js
@@ -2,8 +2,10 @@ import { connect } from 'react-redux'
 
 import Cart from './../../components/Cart'
 
+const byName = (a, b) => (a.name > b.name) ? 1 : (a.name < b.name) ? -1 : 0
+
 const mapStateToProps = (state) => ({
-    products: state.cart.products, // TODO - Sort by name
+    products: state.cart.products.slice().sort(byName),
     total: state.cart.total
 })
 
