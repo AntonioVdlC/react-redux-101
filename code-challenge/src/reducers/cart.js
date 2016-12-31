63d1eedf866f230e0db99e9b048f52d67b6bd7b3
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-12-31 19:57:53 +0100

    Recalculate all unit prices

--- code-challenge/src/reducers/cart.js
+++ code-challenge/src/reducers/cart.js
@@ -5,18 +5,41 @@ const initialState = {
     total: 0,
 }
 
-function getTotal(cart) {
-    return cart.reduce((total, product) => total + product.unitPrice, 0)
+const getTotal = (cart) =>
+    cart.reduce((total, product) => total + product.unitPrice, 0)
+
+const pricingRules = {
+    unit: ({ unitPrice }) => unitPrice,
+    bulk: ({ id, unitPrice, bulkNumber, bulkPrice }, cart) => {
+        let count = cart.reduce((t, p) => (p.id === id) ? ++t : t, 0)
+        return (count < bulkNumber) ? unitPrice : bulkPrice
+    },
+    xfory: ({ id, unitPrice, x, y }, cart, index) => {
+        let previousCount = 0
+        for (let i = index - 1; i >= 0; i--) {
+            if (cart[i].id === id) previousCount ++
+        }
+        return (previousCount % x < (x - y)) ? unitPrice : 0
+    },
 }
+
+const applyPricingRules = (cart) =>
+    cart.map((product, index) => ({ ...product,
+        unitPrice: pricingRules[product.type](product, cart, index) }
+    ))
+
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case ADD_TO_CART:
-            // TODO - Recalculate all the unit prices
-            const newCart = [...state.products, action.product]
+            const products = applyPricingRules([
+                ...state.products,
+                action.product
+            ])
+
             return {
-                products: newCart,
-                total: getTotal(newCart),
+                products,
+                total: getTotal(products),
             }
         default:
             return state
